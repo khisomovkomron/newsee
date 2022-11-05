16aa2800eb05f0e8a21bdc5ab4fb9e4714139382
k.hisomov <normok_9595@bk.ru>
2022-11-05 23:41:27 +0300

    router auth: login_access_token, get_current_user, create_token_access

--- routers/auth.py
+++ routers/auth.py
@@ -27,6 +27,10 @@ class CreateUser(BaseModel):
     
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated=['auto'])
 
+models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+
 router = APIRouter(
     prefix='/auth',
     tags=['auth'],
@@ -41,12 +45,56 @@ def get_db():
     finally:
         db.close()
         
+
 def get_hashed_password(password):
+    """hashed proved password"""
     return bcrypt_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    """verifies user password by plain and hashed password"""
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str, db):
+    """returns users if username exists in db and users is verified via password"""
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+    
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+    
+
+def create_access_token(username: str, user_id: int, expired_delta: Optional[timedelta] = None):
+    """returns generated JWT TOKEN using username and user_id, JWT TOKENS is valid for default 15 mins"""
+    encode = {'sub': username, 'id': user_id}
+    if expired_delta:
+        expire = datetime.utcnow() + expired_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({'exp': expire})
+    return jwt.encode(claims=encode, key=SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: str = Depends(oauth2_bearer)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: str = payload.get('id')
+        
+        if username is None or user_id is None:
+            raise get_user_exception()
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise get_user_exception()
+
 
 @router.post('/create/user')
 async def create_new_user(create_user: CreateUser,
                           db: Session = Depends(get_db)):
+    """Create new user: get user model from DB and pass all variables from CreateUser fields to db fields"""
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
@@ -62,4 +110,46 @@ async def create_new_user(create_user: CreateUser,
     db.commit()
     
     return {'status': 'Successful'}
+    
+
+@router.post('/token')
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    """Returns generated token if user authenticated:
+     1. get user username and password
+     2. authenticate user
+     3. generate new token
+     4. return token"""
+    user = authenticate_user(form_data.username, form_data.password, db)
+    
+    if not user:
+        raise token_exception()
+    token_expires = timedelta(minutes=15)
+    
+    token = create_access_token(user.username,
+                                user.id,
+                                expired_delta=token_expires)
+    return {'token': token}
+    
+    
+def get_user_exception():
+    """ returns HTTP exception if users credentials are wrong"""
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Coul not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'}
+    )
+    return credential_exception
+
+
+def token_exception():
+    """returns HTTP exception if provided token by user is invalid"""
+    token_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Incorrect username or password',
+        headers={'WWW-Authenticate': 'Bearer'}
+    )
+    return token_exception
+
+
     
