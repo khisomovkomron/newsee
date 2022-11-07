80eeaeb403312b7d73ddbc7d2edcf47200bf7fe5
k.hisomov <normok_9595@bk.ru>
2022-11-08 00:21:43 +0300

    implemented PUT endpoint for address

--- routers/address.py
+++ routers/address.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from typing import Optional
 from sqlalchemy.orm import Session
 from .auth import get_current_user, get_user_exception
-from .todo import successful_response
+from .todo import successful_response, http_exception
 import models
 from database import SessionLocal, engine
 router = APIRouter(
@@ -85,6 +85,34 @@ async def create_address(address: Address,
     db.commit()
     return successful_response(201)
 
+@router.put('/{address_id}')
+async def update_address(address_id: int,
+                         address: Address,
+                         user: dict = Depends(get_current_user),
+                         db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    
+    address_model = db.query(models.Address)\
+        .filter(models.Address.id == address_id)\
+        .first()
+    
+    if address_model is None:
+        raise http_exception()
+    
+    address_model.address1 = address.address1
+    address_model.address2 = address.address2
+    address_model.city = address.city
+    address_model.state = address.state
+    address_model.country = address.country
+    address_model.postalcode = address.postalcode
+    address_model.apt_num = address.apt_num
+    
+    
+    db.add(address_model)
+    db.commit()
+    
+    return successful_response(200)
 
 @router.delete('/{address_id}')
 async def delete_address(address_id: int,
