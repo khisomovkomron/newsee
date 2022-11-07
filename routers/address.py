b2c055bd9a795e382a9d076900fc09c5952a0b17
k.hisomov <normok_9595@bk.ru>
2022-11-07 23:21:19 +0300

    implemented create_address API

--- routers/address.py
+++ routers/address.py
@@ -5,10 +5,10 @@ from fastapi import Depends, APIRouter
 from pydantic import BaseModel
 from typing import Optional
 from sqlalchemy.orm import Session
-from auth import get_current_user, get_user_exception
+from .auth import get_current_user, get_user_exception
+from .todo import successful_response
 import models
 from database import SessionLocal, engine
-
 router = APIRouter(
     prefix='/address',
     tags=['addres'],
@@ -23,4 +23,39 @@ def get_db():
     finally:
         db.close()
         
-        
+class Address(BaseModel):
+    address1: str
+    address2: Optional[str]
+    city: str
+    state: str
+    country: str
+    postalcode: str
+    apt_num: Optional[int]
+    
+
+@router.post('/')
+async def create_address(address: Address,
+                         user: dict = Depends(get_current_user),
+                         db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    
+    address_model = models.Address()
+    address_model.address1 = address.address1
+    address_model.address2 = address.address2
+    address_model.city = address.city
+    address_model.state = address.state
+    address_model.country = address.country
+    address_model.postalcode = address.postalcode
+    address_model.apt_num = address.apt_num
+    
+    db.add(address_model)
+    db.flush()
+    
+    user_model = db.query(models.Users).filter(models.Users.id == user.get('id')).first()
+    
+    user_model.address_id = address_model.id
+    
+    db.add(user_model)
+    db.commit()
+    return successful_response(201)
